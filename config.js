4197d113d4f9514add4ef604b2f15a30c0c9f112
Barret <jianhong.liou@gmail.com>
2022-12-13 19:11:53 +0800

    change config

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// const path = "https://json-server-vercel-three.vercel.app/";
-const path = "http://localhost:3000/";
+const path = "https://json-server-vercel-three.vercel.app/";
+// const path = "http://localhost:3000/";
